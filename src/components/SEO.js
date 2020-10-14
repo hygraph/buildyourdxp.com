c804d14625726699d72acf177c0437b31218b3fe
Larisa Christie <larisa@graphcms.com>
2020-10-14 15:56:11 +0200

    fix: og type name changed to property

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -53,7 +53,7 @@ export default function SEO({
       <meta property="og:site_name" content="Build Your DXP" />
       <meta property="og:image" content={pageOgImage} />
       <meta property="og:image:type" content="image/png" />
-      <meta name="og:type" content="website" />
+      <meta property="og:type" content="website" />
 
       <meta name="twitter:title" content={pageTitle} />
       <meta name="twitter:card" content="summary_large_image" />
